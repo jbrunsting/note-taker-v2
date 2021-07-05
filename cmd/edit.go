f107160210b7a7fb972beeeef207fd5c4b3d26d0
Jacob Brunsting <jbrunsting@uwaterloo.ca>
2021-07-05 16:14:18 -0400

    Add prompt to select file to edit

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/jbrunsting/note-taker-v2/editor"
 	"github.com/jbrunsting/note-taker-v2/utils"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -26,7 +33,75 @@ func init() {
 	rootCmd.AddCommand(editCmd)
 }
 
+type searchOption struct {
+	Name    string
+	Preview string
+}
+
+func getSearchOptions(dir string) ([]searchOption, error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return []searchOption{}, err
+	}
+	searchOptions := []searchOption{}
+	for _, f := range files {
+		name := f.Name()
+		path := fmt.Sprintf("%v/%v", dir, name)
+		file, err := os.Open(path)
+		if err != nil {
+			return []searchOption{}, err
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		preview := ""
+		lines := 0
+		for scanner.Scan() {
+			preview = preview + scanner.Text() + "\n"
+			lines += 1
+			if lines > 5 {
+                preview = preview + "..." + "\n"
+				break
+			}
+		}
+		searchOptions = append(searchOptions, searchOption{Name: name, Preview: preview})
+	}
+	return searchOptions, nil
+}
+
 func searchAndEdit(dir string) error {
-    title := "test"
-	return editor.Edit(dir, title)
+	files, err := getSearchOptions(dir)
+	if err != nil {
+		return err
+	}
+	templates := &promptui.SelectTemplates{
+        Label:    "Notes:",
+		Active:   "> {{ .Name | green }}",
+		Inactive: "  {{ .Name | cyan }}",
+		Selected: "> {{ .Name | green }}",
+		Details: `
+{{ .Preview | faint }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		pepper := files[index]
+		name := strings.Replace(strings.ToLower(pepper.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+        Label:             "Note",
+		Items:             files,
+		Templates:         templates,
+		Size:              15,
+		Searcher:          searcher,
+		StartInSearchMode: true,
+	}
+	i, _, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
+	return editor.Edit(dir, files[i].Name)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/jbrunsting/note-taker-v2
 go 1.14
 
 require (
+	github.com/c-bata/go-prompt v0.2.6
+	github.com/manifoldco/promptui v0.8.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
